095918b2700cac12bb7691124721091d7b677573
SeniorIntern <tnikhil.official@gmail.com>
2024-02-02 23:05:56 +0545

    Create a router

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import todosRouter from './routers/todos';
+
 const app = express();
+
+app.use('/todos', todosRouter);
 
 app.listen(8000, () => console.log('Server started'))
