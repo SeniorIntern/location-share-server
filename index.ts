e955edf5a1c31cb590d1c712ce09205bb53f8ab1
SeniorIntern <tnikhil.official@gmail.com>
2024-02-03 12:26:38 +0545

    Set up mongoDB

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
-import todosRouter from './routers/todos';
+import locationRouter from './routers/location';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
+import cors from 'cors';
 
 const app = express();
+config()
+
+mongoose.connect(process.env.URI!)
+  .then(() => console.log('MongoDB connection established'))
+  .catch(err => console.log(err.message))
 
 app.use(express.json());
-app.use('/todos', todosRouter);
+app.use(cors());
+app.use('/api/location', locationRouter);
 
-app.listen(8000, () => console.log('Server started'));
+const port = process.env.PORT;
+app.listen(port, () => console.log(`connected on port: ${port}`));
